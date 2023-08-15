6915c951e3aeeff97c710dea1febf2b8dead6630
Miguel <md9914461@gmail.com>
2023-08-15 11:14:55 -0300

    Agregado de pagina de registro y login

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,12 +24,17 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import db
-    db.init_app(app)
-
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+
+    from . import db
+    db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+
 
     return app
