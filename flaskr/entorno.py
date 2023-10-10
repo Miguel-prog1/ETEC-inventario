2b1cb8061a6e2e97a20864bd8f895d843684a4d9
Andres Navarro <canavarro82@gmail.com>
2023-10-10 10:48:16 -0300

    Quité la estructura en sectores de la respuesta de entornos

--- flaskr/entorno.py
+++ flaskr/entorno.py
@@ -10,22 +10,7 @@ def index():
         """SELECT e.id, e.nombre, s.nombre AS peine, s.piso FROM entornos e 
         JOIN sector s ON s.id = e.sector ORDER BY peine, piso, e.nombre"""
     ).fetchall()
-    # voy a armar un diccionario que tenga como claves los peines de la escuela
-    # los valores de cada uno serán un diccionario que tiene como clave los pisos
-    # y como valores una lista de los entornos formativos de cada piso
-    peines = {}
-    for e in entornos:
-        peine = e["peine"]
-        piso = e["piso"]
-        if peine not in peines:
-            peines[peine] = {}
-        pisosDelPeine = peines[peine]
-        if piso not in pisosDelPeine:
-            pisosDelPeine[piso] = []
-        listaDeEntornos = pisosDelPeine[piso]
-        listaDeEntornos.append(e)        
-    print(peines)
-    return jsonify(peines=peines)
+    return jsonify(entornos=entornos)
 
 @bp.route('/detalle/<string:nombre>/')
 def detail(nombre):
