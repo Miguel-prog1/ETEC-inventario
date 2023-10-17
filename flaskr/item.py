146f1d564f47facbffe1a26f9eb05aa00a916de3
miguel-prog1 <md9914461@gmail.com>
2023-10-17 13:28:29 -0300

    hice el delate de los items

--- flaskr/item.py
+++ flaskr/item.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, jsonify, request, url_for)
+from flask import (Blueprint, abort, jsonify, request, url_for)
 from flaskr.db import get_db
 
 bp = Blueprint('item', __name__, url_prefix='/api/items')
@@ -24,11 +24,13 @@ def index():
         return jsonify(items=items)
 
 
-@bp.route('/<int:id>', methods=('GET', 'PUT'))
+@bp.route('/<int:id>', methods=('GET', 'PUT', 'DELETE'))
 def detail(id):
     db = get_db()
     item = db.execute(
         'SELECT id, nombre FROM items WHERE id = ?', (id,)).fetchone()
+    if item == None:
+        abort(404,'No existe este item')
     if request.method == 'PUT':
         item = request.json['item']
         nombre = item['nombre']
@@ -36,5 +38,12 @@ def detail(id):
             'UPDATE items SET nombre = ? WHERE id = ?', (nombre, id))
         db.commit()
         return jsonify(item=url_for('item.detail', id=id))
+    elif request.method == 'DELETE':
+        db.execute(
+            'DELETE FROM items WHERE id = ?', (id,)
+        )
+        db.commit()
+        return jsonify(id=id)
     elif request.method == 'GET':
         return jsonify(item=item)
+
