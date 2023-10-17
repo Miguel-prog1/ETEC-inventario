18bd096ed0e0766638d290cf8d5495be2c6dd8ba
jols0 <marianacademico1@gmail.com>
2023-10-17 13:18:44 -0300

    agregue metodos de la api para agregar listar y modificar items

--- flaskr/item.py
+++ flaskr/item.py
@@ -1,11 +1,40 @@
-from flask import (Blueprint, jsonify)
+from flask import (Blueprint, jsonify, request, url_for)
 from flaskr.db import get_db
 
-bp = Blueprint('item', __name__, url_prefix='/api/item')
+bp = Blueprint('item', __name__, url_prefix='/api/items')
 
-@bp.route('/detalle/<int:id>')
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    db = get_db()
+    if request.method == 'POST':
+        item = request.json['item']
+        nombre = item['nombre']
+        res = db.execute(
+            """INSERT INTO items(nombre) 
+                VALUES (?)""",
+            (nombre, )
+        )
+        id = res.lastrowid
+        db.commit()
+        return jsonify(item=url_for('item.detail', id=id))
+    elif request.method == 'GET':
+        items = db.execute(
+            'SELECT id, nombre FROM items').fetchall()
+        return jsonify(items=items)
+
+
+@bp.route('/<int:id>', methods=('GET', 'PUT'))
 def detail(id):
     db = get_db()
     item = db.execute(
         'SELECT id, nombre FROM items WHERE id = ?', (id,)).fetchone()
-    return jsonify(item=item)
+    if request.method == 'PUT':
+        item = request.json['item']
+        nombre = item['nombre']
+        db.execute(
+            'UPDATE items SET nombre = ? WHERE id = ?', (nombre, id))
+        db.commit()
+        return jsonify(item=url_for('item.detail', id=id))
+    elif request.method == 'GET':
+        return jsonify(item=item)
